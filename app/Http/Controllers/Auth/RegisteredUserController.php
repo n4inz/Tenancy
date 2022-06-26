bc0105ab998ec4c4afafed8db59eb7440ec0ba04
n4inz <nainmo@ymail.com>
2022-06-25 23:01:57 -0700

    tenancy

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Tenant;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Str;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'sub_domain' => ['required', 'min:3'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+   
+
+        $tenant = Tenant::create([
+            'id' => $request->sub_domain,
+        ]);
+        $tenant->domains()->create(['domain' => $request->sub_domain.'.laravel-pertama.test']);
+        $user = $tenant->user()->create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+        ]);
+
+ 
+
+   
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+      
+        $main = str_replace('://', '://'. $request->sub_domain . '.' , config('app.url'));
+
+       
+        return redirect(to:$main . RouteServiceProvider::HOME);
+    }
+}
